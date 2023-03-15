0f22742a53941d0449df9337abf790dbaefd0e63
gimminjong <alswhd1113@gmail.com>
2023-03-15 14:10:00 +0900

    chore(PostAppService) : 데이터 조회 로직을 메소드로 분리
    CRUD 과정에서 post, member, anonymous 의 조회와 null 체크 로직이 반복되기 때문에 메소드를 분리
    각 private 메소드는 대상을 조회한 뒤 empty 상태이면 예외를 발생
    
    addition :
        private Member findMemberById(Long memberId)
        private Post findPostById(Long postId)
        private Anonymous findAnonymousById(Long anonymousId)

--- seboard/src/main/java/com/seproject/seboard/application/PostAppService.java
+++ seboard/src/main/java/com/seproject/seboard/application/PostAppService.java
@@ -2,7 +2,6 @@ package com.seproject.seboard.application;
 
 import com.seproject.seboard.domain.model.Post;
 import com.seproject.seboard.domain.model.author.Anonymous;
-import com.seproject.seboard.domain.model.author.Author;
 import com.seproject.seboard.domain.model.author.Member;
 import com.seproject.seboard.domain.repository.AnonymousRepository;
 import com.seproject.seboard.domain.repository.MemberRepository;
@@ -45,15 +44,7 @@ public class PostAppService {
 
     public void createNamedPost(String title, String contents, Long categoryId, Long userId){
         validatePost(title,contents,categoryId);
-        Optional<Member> memberRepositoryById = memberRepository.findById(userId);
-
-        //멤버가 존재하는지 TODO : message
-        if(memberRepositoryById.isEmpty()) {
-            throw new NoSuchElementException();
-        }
-
-        //멤버 추출
-        Member member = memberRepositoryById.get();
+        Member member = findMemberById(userId);
 
         Post post = Post.builder()
                 .title(title)
@@ -64,25 +55,11 @@ public class PostAppService {
 
         postRepository.save(post);
     }
-
     public void deleteNamedPost(Long postId, Long userId ) {
         // 권한 체크 -> 메소드 보안 체크 가능함 : AOP로 처리
         // 시큐리티 클라우드 적용 방법 -> 게이트웨이에 적용 가능
-        Optional<Post> post = postRepository.findById(postId);
-
-        //게시물이 존재하지 않는 경우 TODO : message
-        if(post.isEmpty()) {
-            throw new NoSuchElementException();
-        }
-
-        Optional<Member> foundMember = memberRepository.findById(userId);
-
-        //작성한 게시글의 작성자가 존재하지 않는 경우 TODO : message
-        if(foundMember.isEmpty()) {
-            throw new NoSuchElementException();
-        }
-        Post targetPost = post.get();
-        Member targetMember = foundMember.get();
+        Post targetPost = findPostById(postId);
+        Member targetMember = findMemberById(userId);
 
         //작성한 게시글의 작성자가 존재하지 않는 경우 TODO : message
         if(!targetPost.isSameAuthor(targetMember)) {
@@ -95,24 +72,12 @@ public class PostAppService {
     public void deleteUnnamedPost(Long postId, Long userId, String password) {
         // 권한 체크 -> 메소드 보안 체크 가능함 : AOP로 처리
         // 시큐리티 클라우드 적용 방법 -> 게이트웨이에 적용 가능
-        Optional<Post> post = postRepository.findById(postId);
 
-        //게시물이 존재하지 않는 경우 TODO : message
-        if(post.isEmpty()) {
-            throw new NoSuchElementException();
-        }
 
-        Optional<Anonymous> foundAnonymous = anonymousRepository.findById(userId);
+        Post targetPost = findPostById(postId);
+        Anonymous targetAnonymous = findAnonymousById(userId);
 
-        //작성한 게시글의 작성자가 존재하지 않는 경우 TODO : message
-        if(foundAnonymous.isEmpty()) {
-            throw new NoSuchElementException();
-        }
-
-        Post targetPost = post.get();
-        Anonymous targetAnonymous = foundAnonymous.get();
-
-        //작성한 게시글의 작성자가 존재하지 않는 경우 TODO : message
+        //작성한 게시글의 작성자가 서로 다른 경우 TODO : message
         if(!targetPost.isSameAuthor(targetAnonymous)) {
             throw new IllegalArgumentException();
         }
@@ -123,6 +88,46 @@ public class PostAppService {
 
         postRepository.deleteById(postId);
     }
+
+    // 게시물을 찾고 존재하지 않으면 예외를 전달
+    private Post findPostById(Long postId){
+        Optional<Post> post = postRepository.findById(postId);
+
+        //게시물이 존재하지 않는 경우 TODO : message
+        if(post.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        Post targetPost = post.get();
+        return targetPost;
+    }
+
+
+    //사용자를 찾고 존재하지 않으면 예외를 전달하는 함수 -> 하나로 함치는 방법 고려
+    private Member findMemberById(Long memberId){
+        Optional<Member> foundMember = memberRepository.findById(memberId);
+
+        //작성한 게시글의 작성자가 존재하지 않는 경우 TODO : message
+        if(foundMember.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        Member targetMember = foundMember.get();
+        return targetMember;
+    }
+
+    private Anonymous findAnonymousById(Long anonymousId){
+        Optional<Anonymous> foundAnonymous = anonymousRepository.findById(anonymousId);
+
+        //작성한 게시글의 작성자가 존재하지 않는 경우 TODO : message
+        if(foundAnonymous.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        Anonymous targetAnonymous = foundAnonymous.get();
+        return targetAnonymous;
+    }
+
 
     private void validatePost(String title,String contents,Long categoryId){
 
