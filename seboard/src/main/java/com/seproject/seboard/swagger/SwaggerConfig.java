c81208aad132a4a205da129c5321f2e2cf7bf942
gimminjong <alswhd1113@gmail.com>
2023-03-27 18:24:42 +0900

    feat(SwaggerConfig) : 스웨거 API 추가

---
+++ seboard/src/main/java/com/seproject/seboard/swagger/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.seproject.seboard.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("SEBoard-v1")
+                .pathsToMatch("/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("SE 게시판 API")
+                        .description("SE Board Swagger API")
+                        .version("v1"));
+    }
+
+}
