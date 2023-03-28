6a474db64bc08754373897ff2e78c491a950011c
gimminjong <alswhd1113@gmail.com>
2023-03-29 00:25:26 +0900

    feat(CategoryController) : 카테고리 컨트롤러 추가
    카테고리 관련 API 명세를 보고 controller와 request 클래스 구현

--- seboard/src/main/java/com/seproject/seboard/controller/CategoryController.java
+++ seboard/src/main/java/com/seproject/seboard/controller/CategoryController.java
@@ -1,40 +1,64 @@
 package com.seproject.seboard.controller;
 
-import com.seproject.seboard.application.CategoryAppService;
-import com.seproject.seboard.dto.CategoryDTO;
-import com.seproject.seboard.dto.MessageDTO.ResponseMessageDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static com.seproject.seboard.dto.post.CategoryRequest.*;
 
 @RestController
 @RequestMapping("/category")
 @AllArgsConstructor
 public class CategoryController {
-    private final CategoryAppService categoryAppService;
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseMessageDTO enrollCategory(@RequestBody CategoryDTO.CategoryRequestDTO dto, Long userId){
-        categoryAppService.createCategory(dto.getSuperCategoryId(), userId, dto.getName());
-        return new ResponseMessageDTO(""); //TODO : message
+    public ResponseEntity<?> createCategory(@RequestBody CreateCategoryRequest request) {
+
+        /**
+         * TODO : jwt
+         *         존재하지 않는 상위 카테고리
+         *         이미 존재하는 카테고리 이름
+         *         권한 없음
+         */
+
+        return new ResponseEntity<>(request, HttpStatus.OK);
     }
 
-    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<?> retrieveCategoryList(@PathVariable Long categoryId) {
+
+        /**
+         * TODO :
+         *    존재하지 않는 대분류 카테고리
+         */
+
+        return new ResponseEntity<>(categoryId, HttpStatus.OK);
+    }
+
     @PutMapping("/{categoryId}")
-    public ResponseMessageDTO updateCategory(@RequestBody CategoryDTO.CategoryRequestDTO dto,
-                                             @PathVariable Long categoryId, Long userId){
-        categoryAppService.updateCategory(categoryId, userId, dto.getName());
-        return new ResponseMessageDTO(""); // TODO : message
+    public ResponseEntity<?> updateCategoryList(@PathVariable Long categoryId, @RequestBody UpdateCategoryRequest request) {
+
+        /**
+         * TODO : jwt
+         *    권한 없음
+         *    존재하지 않는 상위 카테고리
+         *    이미 존재하는 카테고리 이름
+         */
+
+        return new ResponseEntity<>(request, HttpStatus.OK);
     }
 
-    @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping("/{categoryId}")
-    public ResponseMessageDTO removeCategory(@PathVariable Long categoryId, Long userId){
-        categoryAppService.removeCategory(categoryId, userId);
-        return new ResponseMessageDTO(""); // TODO : message
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<?> deleteCategoryList(@PathVariable Long categoryId) {
+
+        /**
+         * TODO : jwt
+         *    권한 없음
+         *    대분류 삭제시 하위 카테고리 없어야함
+         */
+
+        return new ResponseEntity<>(categoryId, HttpStatus.OK);
     }
-
-
 
 }
