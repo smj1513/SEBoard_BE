dc3cdac3bfbf08d57f6933fbb5b98840ccfe1441
gimminjong <alswhd1113@gmail.com>
2023-03-29 00:38:55 +0900

    feat(BookmarkController) : bookmark API swagger 추가
    swagger에 명시되는 설명, 제목 등을 추가하는 어노테이션 작성
    
    addition:
        @Tag(name = "API 이름", description = "API에 대한 간략한 설명")
        @Operation(summary = "해당 메소드가 무슨 역할 하는지",
                    description = "메소드의 시그니처 간략한 설명")

--- seboard/src/main/java/com/seproject/seboard/controller/BookmarkController.java
+++ seboard/src/main/java/com/seproject/seboard/controller/BookmarkController.java
@@ -1,19 +1,20 @@
 package com.seproject.seboard.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("bookmark/posts/{postId}/")
-@AllArgsConstructor
+@Tag(name = "북마크 API", description = "게시물 즐겨찾기 관련 API")
 public class BookmarkController {
 
     @Operation(summary = "게시글 즐겨찾기 등록",
-            description = "사용자가 게시글을 북마크 등록 요청",
-            tags = { "Bookmark Controller" })
+            description = "사용자가 게시글을 북마크 등록 요청")
     @PostMapping
     public ResponseEntity<?> createBookmark(@PathVariable Long postId){
         /**
@@ -23,6 +24,8 @@ public class BookmarkController {
         return new ResponseEntity<>(postId,HttpStatus.OK);
     }
 
+    @Operation(summary = "게시글 즐겨찾기 해제",
+            description = "사용자가 게시글을 북마크 해제 요청")
     @DeleteMapping
     public ResponseEntity<?> cancelBookmark(@PathVariable Long postId){
         /**
