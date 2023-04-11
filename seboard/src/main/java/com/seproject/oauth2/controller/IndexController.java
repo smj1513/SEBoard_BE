8bc699a70f311e34af0ff904b5afdd292444bbd4
ma-an-jong <alswhd1113@gmail.com>
2023-04-11 17:37:07 +0900

    refactor(converter) : 사용하지 않는 converter 삭제

--- seboard/src/main/java/com/seproject/oauth2/controller/IndexController.java
+++ seboard/src/main/java/com/seproject/oauth2/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.seproject.oauth2.controller;
 
 import com.seproject.oauth2.model.ProviderUser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -9,14 +10,16 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @Slf4j
 @RestController
 public class IndexController {
 
     @GetMapping("/")
-    public ResponseEntity<?> index(Authentication authentication, @AuthenticationPrincipal ProviderUser providerUser) {
-        return new ResponseEntity<>(providerUser,HttpStatus.OK);
-
+    public ResponseEntity<?> index(HttpServletRequest request, HttpServletResponse response) {
+        return new ResponseEntity<>(request,HttpStatus.OK);
     }
 }
