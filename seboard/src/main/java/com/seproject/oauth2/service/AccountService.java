780293a221390cfd3793e190c4653f7bb81b2abc
ma-an-jong <alswhd1113@gmail.com>
2023-04-24 19:48:58 +0900

    feat(AccountService) : form 회원가입 기능 추가

--- seboard/src/main/java/com/seproject/oauth2/service/AccountService.java
+++ seboard/src/main/java/com/seproject/oauth2/service/AccountService.java
@@ -1,19 +1,21 @@
 package com.seproject.oauth2.service;
 
+import com.seproject.oauth2.controller.command.AccountRegisterCommand;
 import com.seproject.oauth2.controller.command.OAuthAccountCommand;
-import com.seproject.oauth2.model.social.AbstractOidcUser;
+import com.seproject.oauth2.model.EmailAuthentication;
 import com.seproject.oauth2.repository.AccountRepository;
 import com.seproject.oauth2.repository.RoleRepository;
 import com.seproject.oauth2.model.Account;
 import com.seproject.oauth2.model.Role;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,6 +24,7 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public boolean isExist(String loginId){
         return accountRepository.existsByLoginId(loginId);
@@ -32,7 +35,7 @@ public class AccountService {
         List<Role> authorities = List.of(roleUser.get());
 
         Account account = Account.builder()
-                .loginId(accountCommand.getProvider() + "_" +accountCommand.getId())
+                .loginId(accountCommand.getId())
                 .provider(accountCommand.getProvider())
                 .username(accountCommand.getName())
                 .nickname(accountCommand.getNickname())
@@ -47,6 +50,25 @@ public class AccountService {
 
     public Account findAccountById(String loginId) {
         return accountRepository.findByLoginId(loginId);
+    }
+
+    @Transactional
+    public void register(AccountRegisterCommand accountRegisterCommand) {
+        Optional<Role> roleUser = roleRepository.findByName("ROLE_KUMOH");
+        List<Role> authorities = List.of(roleUser.get());
+
+        Account account = Account.builder()
+                .loginId(accountRegisterCommand.getId())
+                .provider("se")
+                .username(accountRegisterCommand.getName())
+                .nickname(accountRegisterCommand.getNickname())
+                .password(passwordEncoder.encode(accountRegisterCommand.getPassword()))
+                .email(accountRegisterCommand.getId())
+                .profile("none")
+                .authorities(authorities)
+                .build();
+
+        accountRepository.save(account);
     }
 
 }
