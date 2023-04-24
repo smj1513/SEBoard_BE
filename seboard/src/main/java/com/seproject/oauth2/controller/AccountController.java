4e9af62744b3913b197ffc4a333e3ff24eef0761
ma-an-jong <alswhd1113@gmail.com>
2023-04-24 19:50:44 +0900

    feat(EmailService) : 이메일 인증 기능 추가

--- seboard/src/main/java/com/seproject/oauth2/controller/AccountController.java
+++ seboard/src/main/java/com/seproject/oauth2/controller/AccountController.java
@@ -1,8 +1,12 @@
 package com.seproject.oauth2.controller;
 
 import com.seproject.oauth2.application.LogoutAppService;
+import com.seproject.oauth2.controller.command.AccountRegisterCommand;
 import com.seproject.oauth2.controller.command.OAuthAccountCommand;
+import com.seproject.oauth2.controller.dto.EmailAuthenticationRequest;
+import com.seproject.oauth2.controller.dto.EmailConfirmRequestDTO;
 import com.seproject.oauth2.service.AccountService;
+import com.seproject.oauth2.service.EmailService;
 import com.seproject.oauth2.utils.jwt.JwtDecoder;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.NoSuchElementException;
+
 import static com.seproject.oauth2.controller.dto.RegisterDTO.*;
 
 @AllArgsConstructor
@@ -25,6 +31,7 @@ public class AccountController {
     private final JwtDecoder jwtDecoder;
     private final LogoutAppService logoutAppService;
     private final AccountService accountService;
+    private final EmailService emailService;
 
     @PostMapping("/account/oauth")
     public ResponseEntity<?> registerUserWithOAuth(HttpServletRequest request,@RequestBody OAuth2RegisterRequest oAuth2RegisterRequest) {
@@ -51,9 +58,44 @@ public class AccountController {
     }
 
     @PostMapping("/account/form")
-    public ResponseEntity<?> registerUserWithForm() {
+    public ResponseEntity<?> registerUserWithForm(@RequestBody FormRegisterRequest formRegisterRequest) {
 
-        return new ResponseEntity<>("회원가입 완료",HttpStatus.OK);
+        String id = formRegisterRequest.getId();
+
+        boolean confirmed = emailService.isConfirmed(id);
+
+        if(confirmed) {
+            accountService.register(AccountRegisterCommand.builder()
+                    .id(id)
+                    .password(formRegisterRequest.getPassword())
+                    .name(formRegisterRequest.getName())
+                    .nickname(formRegisterRequest.getNickname()).build());
+            return new ResponseEntity<>("가입 완료",HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("인증되지 않은 이메일입니다.",HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/email/auth")
+    public ResponseEntity<?> sendEmail(@RequestBody EmailAuthenticationRequest request) {
+
+        if(accountService.isExist(request.getEmail())) {
+            return new ResponseEntity<>("이미 인증된 이메일입니다." , HttpStatus.BAD_REQUEST);
+        }
+
+        emailService.send(request.getEmail());
+        return new ResponseEntity<>("입력한 이메일로 인증 코드를 전송했습니다.",HttpStatus.OK);
+    }
+
+    @PostMapping("/email/confirm")
+    public ResponseEntity<?> confirmAuthCode(@RequestBody EmailConfirmRequestDTO emailConfirmDTO) {
+        try {
+            emailService.confirm(emailConfirmDTO.getEmail(), emailConfirmDTO.getAuthToken());
+            return new ResponseEntity<>("인증 성공",HttpStatus.OK);
+        }
+        catch (NoSuchElementException e) {
+            return new ResponseEntity<>("일치하지 않는 정보",HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/logoutProc") //미해결
---
+++ seboard/src/main/java/com/seproject/oauth2/repository/EmailAuthRepository.java
@@ -0,0 +1,16 @@
+package com.seproject.oauth2.repository;
+
+import com.seproject.oauth2.model.EmailAuthentication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface EmailAuthRepository extends JpaRepository<EmailAuthentication,Long> {
+
+    @Query(value = "select * from email_authentication where email = :email and auth_token = :authToken",nativeQuery = true)
+    EmailAuthentication findEmailAuthentication(String email, String authToken);
+
+    @Query(value = "select * from email_authentication where email = :email",nativeQuery = true)
+    EmailAuthentication findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
