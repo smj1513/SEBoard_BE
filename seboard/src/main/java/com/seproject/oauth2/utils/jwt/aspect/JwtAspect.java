4b2894787874f652a891f20ada3f96f084e0bf30
ma-an-jong <alswhd1113@gmail.com>
2023-04-24 19:47:05 +0900

    feat(jwt) : 인증 객체 종류 변경으로 인한 jwt 일부 수정

--- seboard/src/main/java/com/seproject/oauth2/utils/jwt/aspect/JwtAspect.java
+++ seboard/src/main/java/com/seproject/oauth2/utils/jwt/aspect/JwtAspect.java
@@ -1,14 +1,9 @@
 package com.seproject.oauth2.utils.jwt.aspect;
 
-import com.seproject.oauth2.utils.jwt.JwtDecoder;
-import lombok.AllArgsConstructor;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.security.web.access.expression.ExpressionBasedFilterInvocationSecurityMetadataSource;
 
 import javax.security.sasl.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
