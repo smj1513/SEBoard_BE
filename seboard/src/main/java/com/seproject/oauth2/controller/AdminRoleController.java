690ef8259ebfaa8bd13b67079c1143d642f3e8f1
ma-an-jong <alswhd1113@gmail.com>
2023-04-26 23:53:09 +0900

    feat(RoleController) : Role 목록 조회 기능 추가

---
+++ seboard/src/main/java/com/seproject/oauth2/controller/AdminRoleController.java
@@ -0,0 +1,91 @@
+package com.seproject.oauth2.controller;
+
+import com.seproject.oauth2.controller.dto.AccountDTO;
+import com.seproject.oauth2.service.RoleService;
+import com.seproject.oauth2.utils.jwt.JwtDecoder;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.seproject.oauth2.controller.dto.RoleDTO.*;
+
+@Tag(name = "권한 관리 API", description = "관리자 시스템의 권한 관리 API")
+@AllArgsConstructor
+@RequestMapping(value = "/admin")
+@Controller
+public class AdminRoleController {
+
+    private final JwtDecoder jwtDecoder;
+    private final RoleService roleService;
+
+    @Operation(summary = "권한 목록 조회", description = "등록된 권한 목록들을 조회한다.")
+    @ApiResponses({
+            @ApiResponse(content = @Content(schema = @Schema(implementation = RetrieveAllRoleResponse.class)), responseCode = "200", description = "권한 목록 조회 성공"),
+            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "400", description = "잘못된 페이징 정보")
+    })
+//    @JWT
+    @GetMapping("/roles")
+    public ResponseEntity<?> retrieveAllRole(HttpServletRequest request, @RequestBody RetrieveAllRoleRequest retrieveRoleRequest) {
+//        String jwt = request.getHeader("Authorization");
+//        List<String> authorities = jwtDecoder.getAuthorities(jwt);
+//
+//        if(!authorities.contains("ROLE_ADMIN")) {
+//            return new ResponseEntity<>("접근 권한이 없습니다.",HttpStatus.NOT_ACCEPTABLE);
+//        }
+
+        int page = retrieveRoleRequest.getPage();
+        int perPage = retrieveRoleRequest.getPerPage();
+        page = Math.max(page-1,0);
+        perPage = Math.max(perPage,1);
+
+        try{
+            RetrieveAllRoleResponse roleResponse = roleService.findAll(page, perPage);
+            return new ResponseEntity<>(roleResponse, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("페이지 번호가 잘못되었습니다.",HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @Operation(summary = "권한 생성", description = "새로운 권한을 추가한다.")
+    @ApiResponses({
+            @ApiResponse(content = @Content(schema = @Schema(implementation = AccountDTO.RetrieveAllAccountResponse.class)), responseCode = "200", description = "권한 목록 조회 성공"),
+            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "400", description = "잘못된 페이징 정보")
+    })
+//    @JWT
+    @GetMapping("/roles")
+    public ResponseEntity<?> createRole(HttpServletRequest request, @RequestBody RetrieveAllRoleRequest retrieveRoleRequest) {
+//        String jwt = request.getHeader("Authorization");
+//        List<String> authorities = jwtDecoder.getAuthorities(jwt);
+//
+//        if(!authorities.contains("ROLE_ADMIN")) {
+//            return new ResponseEntity<>("접근 권한이 없습니다.",HttpStatus.NOT_ACCEPTABLE);
+//        }
+
+        int page = retrieveRoleRequest.getPage();
+        int perPage = retrieveRoleRequest.getPerPage();
+        page = Math.max(page-1,0);
+        perPage = Math.max(perPage,1);
+
+        try{
+            RetrieveAllRoleResponse roleResponse = roleService.findAll(page, perPage);
+            return new ResponseEntity<>(roleResponse, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("페이지 번호가 잘못되었습니다.",HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+}
