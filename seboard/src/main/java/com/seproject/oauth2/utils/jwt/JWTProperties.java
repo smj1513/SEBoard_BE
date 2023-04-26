5f64be418f118be2588b3eb2e05245a2f9787f50
ma-an-jong <alswhd1113@gmail.com>
2023-04-26 23:33:57 +0900

    refactor(JWT) : 원시 문자열 wrapping

---
+++ seboard/src/main/java/com/seproject/oauth2/utils/jwt/JWTProperties.java
@@ -0,0 +1,16 @@
+package com.seproject.oauth2.utils.jwt;
+
+public class JWTProperties {
+    public static final String TYPE = "type";
+    public static final String ALGORITHM = "alg";
+    public static final String HS256 = "HS256";
+    public static final String PROVIDER = "provider";
+    public static final String ID = "id";
+    public static final String EMAIL = "email";
+    public static final String PROFILE = "profile";
+    public static final String AUTHORITIES = "authorities";
+    public static final String TEMPORAL_TOKEN = "temporalToken";
+    public static final String REFRESH_TOKEN = "refreshToken";
+    public static final String ACCESS_TOKEN = "accessToken";
+
+}
