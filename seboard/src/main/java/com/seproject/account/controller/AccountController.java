a600e6f2ffe9911c38e90970c92fb4dded146e34
ma-an-jong <alswhd1113@gmail.com>
2023-05-03 00:12:08 +0900

    feat(AccountController) : 로그아웃 기능 구현

--- seboard/src/main/java/com/seproject/account/controller/AccountController.java
+++ seboard/src/main/java/com/seproject/account/controller/AccountController.java
@@ -1,7 +1,9 @@
 package com.seproject.account.controller;
 
 import com.seproject.account.application.LogoutAppService;
+import com.seproject.account.jwt.JwtDecoder;
 import com.seproject.account.service.AccountService;
+import com.seproject.account.service.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
@@ -19,21 +21,25 @@ public class AccountController {
 
     private final LogoutAppService logoutAppService;
     private final AccountService accountService;
+    private final TokenService tokenService;
+    private final JwtDecoder jwtDecoder;
 
-    @Operation(summary = "로그아웃", description = "미해결 상태")
+    @Operation(summary = "로그아웃", description = "로그아웃")
     @GetMapping("/logoutProc") //미해결
     public ResponseEntity<?> logout(Authentication authentication) {
+
         if(authentication == null) {
             return new ResponseEntity<>("로그인 상태가 아닙니다.",HttpStatus.BAD_REQUEST);
         }
 
-        if(authentication instanceof OAuth2AuthenticationToken) {
-            OAuth2AuthenticationToken token = (OAuth2AuthenticationToken)authentication;
-            logoutAppService.logout(token.getAuthorizedClientRegistrationId());
-
-            return new ResponseEntity<>("로그아웃 성공", HttpStatus.OK);
-        }
-
-        return null;
+        String accessToken = jwtDecoder.getAccessToken();
+        tokenService.deleteToken(accessToken);
+        return new ResponseEntity<>("로그아웃 성공", HttpStatus.OK);
+//        if(authentication instanceof OAuth2AuthenticationToken) {
+//            OAuth2AuthenticationToken token = (OAuth2AuthenticationToken)authentication;
+//            logoutAppService.logout(token.getAuthorizedClientRegistrationId());
+//        }
+//
+//        return null;
     }
 }
