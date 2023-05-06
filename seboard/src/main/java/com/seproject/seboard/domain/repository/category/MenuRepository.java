7cf0055689c16e05fceba134690a77b31d48af83
laon <laon198@gmail.com>
2023-05-06 19:46:52 +0900

    feat(Menu) : Menu 조회 로직 구현

--- seboard/src/main/java/com/seproject/seboard/domain/repository/category/MenuRepository.java
+++ seboard/src/main/java/com/seproject/seboard/domain/repository/category/MenuRepository.java
@@ -1,7 +1,13 @@
 package com.seproject.seboard.domain.repository.category;
 
+import com.seproject.seboard.controller.dto.post.CategoryResponse;
 import com.seproject.seboard.domain.model.category.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    List<Menu> findByDepth(int depth);
+    List<Menu> findBySuperMenu(Long superMenuId);
 }
