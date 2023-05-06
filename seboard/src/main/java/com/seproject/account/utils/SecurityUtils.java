601ea6030fb059f20aead504acc14236203f8d27
ma-an-jong <alswhd1113@gmail.com>
2023-05-07 00:06:53 +0900

    feat(SecurityUtils) : 사용자 loginId, 권한 조회 편의 메소드 추가

---
+++ seboard/src/main/java/com/seproject/account/utils/SecurityUtils.java
@@ -0,0 +1,52 @@
+package com.seproject.account.utils;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class SecurityUtils {
+
+
+    /**
+     *  1. 필터에 적용된 구간
+     *  HttpServletRequest -> getHeader("Authorization") -> String으로 된 jwt를 획득
+     *  redis의 토큰 저장소에 토큰 존재 여부 확인 -> 저장된 토큰이 아님(우리가 발행하지 않았거나, 로그아웃 토큰, 만료 토큰 등)
+     *  jwtDecoder -> Subject 파싱 -> loginId 획득 -> AccountService로 사용자를 데이터베이스에서 조회
+     *  User 객체 생성(loginId => getUsername() , 권한 정보 => getAuthorities()) -> UsernamePassword 토큰으로 SecurityContext에 저장, password는 빈 문자열 저장
+     *  ------------------------------------------------------------------------------------------------------------------------------------
+     *  2. 이후 필요할때 꺼내쓰는 방법
+     *  SecurityContextHolder.getContext().getAuthentication(); -> 저장된 UsernamePassword 토큰 꺼내기,로그인 안한 사용자면 null이 반환 밑에 코드로 참고
+     *  토큰의 getPrincipal() 메소드는 사용자 정보, 권한을 가진 User객체 반환
+     *  User getUsername() -> loginId 조회 -> AccountService를 이용해서 Account 객체 조회
+     *
+     *  getLoginId() -> 사용자의 로그인 아이디 조회
+     *  getAuthorities() -> 사용자가 가진 권한 조회
+     *
+     *  헤더에 Authorization : 토큰 값 넣고 -> http://localhost:8080/parseAccount -> 결과
+     *
+     */
+
+
+    public static String getLoginId(){
+        User user = getUser();
+        if(user == null) return null;
+        return user.getUsername();
+    }
+
+    public static Collection<? extends GrantedAuthority> getAuthorities(){
+        User user = getUser();
+        if(user == null) return null;
+        return user.getAuthorities();
+    }
+
+    private static User getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null) return null;
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = (UsernamePasswordAuthenticationToken) authentication;
+        return (User)usernamePasswordAuthenticationToken.getPrincipal();
+    }
+}
