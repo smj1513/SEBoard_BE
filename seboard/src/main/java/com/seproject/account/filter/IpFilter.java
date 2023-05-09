6902792e43d6e9c06e7df36c4004d8d48cfe0fc4
laon <laon198@gmail.com>
2023-05-09 18:49:48 +0900

    feat(Authorization) : 로그인 인증 여부 필터 수정

--- seboard/src/main/java/com/seproject/account/filter/IpFilter.java
+++ seboard/src/main/java/com/seproject/account/filter/IpFilter.java
@@ -36,29 +36,29 @@ public class IpFilter extends OncePerRequestFilter {
 
         public static String getClientIP(HttpServletRequest request) {
             String ip = request.getHeader("X-Forwarded-For");
-            log.info("> X-FORWARDED-FOR : " + ip);
+//            log.info("> X-FORWARDED-FOR : " + ip);
 
             if (ip == null) {
                 ip = request.getHeader("Proxy-Client-IP");
-                log.info("> Proxy-Client-IP : " + ip);
+//                log.info("> Proxy-Client-IP : " + ip);
             }
             if (ip == null) {
                 ip = request.getHeader("WL-Proxy-Client-IP");
-                log.info(">  WL-Proxy-Client-IP : " + ip);
+//                log.info(">  WL-Proxy-Client-IP : " + ip);
             }
             if (ip == null) {
                 ip = request.getHeader("HTTP_CLIENT_IP");
-                log.info("> HTTP_CLIENT_IP : " + ip);
+//                log.info("> HTTP_CLIENT_IP : " + ip);
             }
             if (ip == null) {
                 ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-                log.info("> HTTP_X_FORWARDED_FOR : " + ip);
+//                log.info("> HTTP_X_FORWARDED_FOR : " + ip);
             }
             if (ip == null) {
                 ip = request.getRemoteAddr();
-                log.info("> getRemoteAddr : "+ip);
+//                log.info("> getRemoteAddr : "+ip);
             }
-            log.info("> Result : IP Address : "+ip);
+//            log.info("> Result : IP Address : "+ip);
 
             return ip;
         }
