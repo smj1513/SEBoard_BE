2a48f981125e2373f6e973224be91ee0e710dd62
ma-an-jong <alswhd1113@gmail.com>
2023-05-13 18:58:56 +0900

    feat(KumohEmailService) : 정회원 인증 이메일 서비스 추가

--- seboard/src/main/java/com/seproject/account/controller/EmailController.java
+++ seboard/src/main/java/com/seproject/account/controller/EmailController.java
@@ -1,6 +1,7 @@
 package com.seproject.account.controller;
 
 import com.seproject.account.service.AccountService;
+import com.seproject.account.service.email.KumohEmailService;
 import com.seproject.account.service.email.PasswordChangeEmailService;
 import com.seproject.account.service.email.RegisterEmailService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -30,6 +31,7 @@ public class EmailController {
 
     private final RegisterEmailService registerEmailService;
     private final PasswordChangeEmailService passwordChangeEmailService;
+    private final KumohEmailService kumohEmailService;
 
     @Parameters(
             {
@@ -66,50 +68,40 @@ public class EmailController {
         return new ResponseEntity<>("인증 성공",HttpStatus.OK);
     }
 
-//    @Parameters(
-//            {
-//                    @Parameter(name = "email", description = "인증번호를 받을 금오 이메일을 전달한다.")
-//            }
-//    )
-//    @Operation(summary = "금오 이메일 인증 요청", description = "회원가입을 위하여 금오 이메일 인증을 진행한다.")
-//    @ApiResponses({
-//            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "200", description = "이메일 전송 성공"),
-//            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "400", description = "잘못된 이메일 형식")
-//    })
-//    @PostMapping("/kumohEmail/auth")
-//    public ResponseEntity<?> sendKumohEmail(@RequestBody EmailAuthenticationRequest request) {
-//
-//        String email = request.getEmail();
-//
-//        if(!registerEmailService.isKumohMail(email)){
-//            return new ResponseEntity<>("잘못된 이메일 형식입니다.", HttpStatus.BAD_REQUEST);
-//        }
-//
-//        registerEmailService.send(email);
-//        return new ResponseEntity<>("입력한 이메일로 인증 코드를 전송했습니다.",HttpStatus.OK);
-//    }
-//
-//    @Parameters(
-//            {
-//                    @Parameter(name = "email", description = "인증번호를 받은 금오 이메일"),
-//                    @Parameter(name = "authToken", description = "인증번호를 입력한다")
-//            }
-//    )
-//    @Operation(summary = "금오 이메일 인증 확인", description = "회원가입을 진행하기 위해 전송한 인증 코드와 이메일이 유효한지 확인한다.")
-//    @ApiResponses({
-//            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "200", description = "이메일 인증 성공"),
-//            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "404", description = "일치하는 정보가 없음"),
-//    })
-//    @PostMapping("/kumohEmail/confirm")
-//    public ResponseEntity<?> confirmKumohAuthCode(@RequestBody EmailConfirmRequest emailConfirmDTO) {
-//        try {
-//            registerEmailService.confirm(emailConfirmDTO.getEmail(), emailConfirmDTO.getAuthToken());
-//            return new ResponseEntity<>("인증 성공",HttpStatus.OK);
-//        }
-//        catch (NoSuchElementException e) {
-//            return new ResponseEntity<>("일치하지 않는 정보",HttpStatus.NOT_FOUND);
-//        }
-//    }
+    @Parameters(
+            {
+                    @Parameter(name = "email", description = "인증번호를 받을 금오 이메일을 전달한다.")
+            }
+    )
+    @Operation(summary = "금오 이메일 인증 요청", description = "정회원 전환을 위하여 금오 이메일 인증을 진행한다.")
+    @ApiResponses({
+            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "200", description = "이메일 전송 성공"),
+            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "400", description = "잘못된 이메일 형식")
+    })
+    @PostMapping("/email/kumoh")
+    public ResponseEntity<?> sendKumohEmail(@RequestBody EmailAuthenticationRequest request) {
+
+        String email = request.getEmail();
+        kumohEmailService.send(email);
+        return new ResponseEntity<>("입력한 이메일로 인증 코드를 전송했습니다.",HttpStatus.OK);
+    }
+
+    @Parameters(
+            {
+                    @Parameter(name = "email", description = "인증번호를 받은 금오 이메일"),
+                    @Parameter(name = "authToken", description = "인증번호를 입력한다")
+            }
+    )
+    @Operation(summary = "금오 이메일 인증 확인", description = "정회원 전환을 위해 전송한 인증 코드와 이메일이 유효한지 확인한다.")
+    @ApiResponses({
+            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "200", description = "이메일 인증 성공"),
+            @ApiResponse(content = @Content(schema = @Schema(implementation = String.class)), responseCode = "404", description = "일치하는 정보가 없음"),
+    })
+    @PostMapping("/email/kumoh/confirm")
+    public ResponseEntity<?> confirmKumohAuthCode(@RequestBody EmailConfirmRequest emailConfirmDTO) {
+        kumohEmailService.confirm(emailConfirmDTO.getEmail(), emailConfirmDTO.getAuthToken());
+        return new ResponseEntity<>("인증 성공",HttpStatus.OK);
+    }
 
 
     @Parameters(
