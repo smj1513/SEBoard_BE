72dc65eb3a88c60df86047fe323df6b35ed8c76b
ma-an-jong <alswhd1113@gmail.com>
2023-05-16 00:33:20 +0900

    feat(LogoutService) : 로그아웃 처리하는 서비스 추가

---
+++ seboard/src/main/java/com/seproject/account/service/LogoutService.java
@@ -0,0 +1,37 @@
+package com.seproject.account.service;
+
+
+import com.seproject.account.jwt.JWT;
+import com.seproject.account.model.token.LogoutRefreshToken;
+import com.seproject.account.model.token.LogoutToken;
+import com.seproject.account.repository.token.LogoutRefreshTokenRepository;
+import com.seproject.account.repository.token.LogoutTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LogoutService {
+
+    private static final String LOGOUT_URL = "https://kauth.kakao.com/oauth/logout?client_id=";
+    private static final String REDIRECT_PATH = "&logout_redirect_uri=http://seboard2.site/login";
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String clientId;
+
+    private final LogoutTokenRepository logoutTokenRepository;
+    private final LogoutRefreshTokenRepository logoutRefreshTokenRepository;
+
+
+    public String getRedirectURL() {
+        return LOGOUT_URL + clientId + REDIRECT_PATH;
+    }
+
+    public void logout(JWT jwt) { //TODO : largeRefreshToken
+        String accessToken = jwt.getAccessToken();
+        String refreshToken = jwt.getRefreshToken();
+        logoutTokenRepository.save(new LogoutToken(accessToken));
+        logoutRefreshTokenRepository.save(new LogoutRefreshToken(refreshToken));
+    }
+
+}
