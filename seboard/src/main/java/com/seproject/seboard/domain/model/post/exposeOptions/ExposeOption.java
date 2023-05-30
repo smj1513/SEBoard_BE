8a82274d99f3e9852fb45d7208393883c89e0d0c
laon <laon198@gmail.com>
2023-05-30 15:31:38 +0900

    feat(Admin Post 조회) : ExposOption 조건 추가

--- seboard/src/main/java/com/seproject/seboard/domain/model/post/exposeOptions/ExposeOption.java
+++ seboard/src/main/java/com/seproject/seboard/domain/model/post/exposeOptions/ExposeOption.java
@@ -8,10 +8,13 @@ import javax.persistence.*;
 @Entity
 @Table(name = "expose_options")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "expose_type")
+@DiscriminatorColumn(name = "expose_type", discriminatorType = DiscriminatorType.STRING)
 public abstract class ExposeOption {
     @Id @GeneratedValue
     private Long exposeOptionId;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "expose_type", insertable = false, updatable = false)
+    protected ExposeState exposeState;
     protected String password;
 
     public abstract ExposeState getExposeState();
