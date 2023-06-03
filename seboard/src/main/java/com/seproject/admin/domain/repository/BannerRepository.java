dc6208baa3b982885bd5e66c50d95c63666364ed
laon <laon198@gmail.com>
2023-06-04 01:48:04 +0900

    feat(Banner 조회) : 메인페이지 Banner 조회 API 구현

--- seboard/src/main/java/com/seproject/admin/domain/repository/BannerRepository.java
+++ seboard/src/main/java/com/seproject/admin/domain/repository/BannerRepository.java
@@ -2,10 +2,13 @@ package com.seproject.admin.domain.repository;
 
 import com.seproject.admin.controller.dto.banner.AdminBannerResponse;
 import com.seproject.admin.domain.Banner;
+import com.seproject.seboard.controller.dto.banner.BannerResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BannerRepository extends JpaRepository<Banner, Long> {
     @Query("select new com.seproject.admin.controller.dto.banner.AdminBannerResponse(b) from Banner b")
@@ -13,8 +16,9 @@ public interface BannerRepository extends JpaRepository<Banner, Long> {
     @Query(value = "select new com.seproject.admin.controller.dto.banner.AdminBannerResponse(b) from Banner b where b.startDate <= current_date and b.endDate >= current_date",
     countQuery = "select count(b) from Banner b where b.startDate <= current_date and b.endDate >= current_date")
     Page<AdminBannerResponse> findActiveBanners(Pageable pageable);
-
     @Query(value = "select new com.seproject.admin.controller.dto.banner.AdminBannerResponse(b) from Banner b where b.startDate > current_date or b.endDate < current_date",
     countQuery = "select count(b) from Banner b where b.startDate > current_date or b.endDate < current_date")
     Page<AdminBannerResponse> findUnActiveBanners(Pageable pageable);
+    @Query("select new com.seproject.seboard.controller.dto.banner.BannerResponse(b) from Banner b where b.startDate <= current_date and b.endDate >= current_date")
+    List<BannerResponse> findBannersForUser();
 }
