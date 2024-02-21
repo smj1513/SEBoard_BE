fc09fd701e643603bb0e9885d1f327a9e109489b
laon198 <laon198@gmail.com>
2024-02-21 21:39:28 +0900

    fix : ExposeOptionRequest 제약조건 변경

--- seboard/src/main/java/com/seproject/board/post/controller/dto/ExposeOptionRequest.java
+++ seboard/src/main/java/com/seproject/board/post/controller/dto/ExposeOptionRequest.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.NotBlank;
 public class ExposeOptionRequest {
     @NotBlank
     private String name;
-    @NotBlank
     private String password = "";
 
     public ExposeOptionRequest(String name) {
