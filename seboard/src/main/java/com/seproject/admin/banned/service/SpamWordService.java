aa5df39fb610c0826afb130fdaef4b100cb4163d
laon198 <laon198@gmail.com>
2024-03-08 21:43:29 +0900

    fix : 관리자 대시보드 일반 API 권한있는 사용자만 이용가능하게 수정

--- seboard/src/main/java/com/seproject/admin/banned/service/SpamWordService.java
+++ seboard/src/main/java/com/seproject/admin/banned/service/SpamWordService.java
@@ -1,15 +1,18 @@
 package com.seproject.admin.banned.service;
 
-import com.seproject.admin.banned.controller.dto.SpamWordRequest;
+import com.seproject.account.account.domain.Account;
+import com.seproject.account.utils.SecurityUtils;
 import com.seproject.admin.banned.controller.dto.SpamWordRequest.SpamWordCreateRequest;
 import com.seproject.admin.banned.controller.dto.SpamWordResponse;
 import com.seproject.admin.banned.domain.SpamWord;
 import com.seproject.admin.banned.domain.repository.SpamWordRepository;
+import com.seproject.admin.dashboard.domain.DashBoardMenu;
+import com.seproject.admin.dashboard.service.AdminDashBoardServiceImpl;
 import com.seproject.error.errorCode.ErrorCode;
+import com.seproject.error.exception.CustomAccessDeniedException;
+import com.seproject.error.exception.CustomAuthenticationException;
 import com.seproject.error.exception.CustomIllegalArgumentException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +24,20 @@ import java.util.List;
 public class SpamWordService {
     private final SpamWordRepository spamWordRepository;
 
+    private final AdminDashBoardServiceImpl dashBoardService;
+
+    //TODO : 추후 AOP로 변경 필요
+    private void checkAuthorization(){
+        Account account = SecurityUtils.getAccount()
+                .orElseThrow(() -> new CustomAuthenticationException(ErrorCode.NOT_LOGIN, null));
+
+        DashBoardMenu dashboardmenu = dashBoardService.findDashBoardMenuByUrl(DashBoardMenu.GENERAL_URL);
+
+        if(!dashboardmenu.authorize(account.getRoles())){
+            throw new CustomAccessDeniedException(ErrorCode.ACCESS_DENIED, null);
+        }
+    }
+
     public List<SpamWordResponse> findAll() {
         //TODO : 개수 제한?
         return spamWordRepository.findAllResponse();
@@ -28,6 +45,8 @@ public class SpamWordService {
 
 
     public void createSpamWord(SpamWordCreateRequest request) {
+        checkAuthorization();
+
         if(spamWordRepository.existsByWord(request.getWord())){
             throw new CustomIllegalArgumentException(ErrorCode.INVALID_REQUEST, null);
         }
@@ -39,6 +58,8 @@ public class SpamWordService {
     }
 
     public void deleteSpamWord(Long id){
+        checkAuthorization();
+
         if(spamWordRepository.findById(id).isEmpty()){
             throw new CustomIllegalArgumentException(ErrorCode.INVALID_REQUEST, null);
         }
